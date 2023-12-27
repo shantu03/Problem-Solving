34276a10dbb38f8646e1730a3e0f111cee93b334
shantu03 <hulwanshantanu@gmail.com>
2023-12-27 18:41:57 +0530

    new

--- Solution2.java
+++ Solution2.java
@@ -1,43 +1,46 @@
+//PARTIALLY ACCEPTED SOLUTION --( PROBLEM IS TIME EXCEED) 
+
+//https://www.hackerearth.com/practice/codemonk/ --> CYCLIC SHIFT PROBLEM 
+// DIFFICULTY- MEDIUM
+
+// USING BIGINTEGER CLASS TO COMPARE ADDITIONAL TEST CASES (WHICH CONTAIN BIG NUMBERS)
+//USING COMPARETO() METHOD
+
 import java.util.Scanner;
-public class Solution2
-{
-
-
-
+import java.math.BigInteger;
+class TestClass {
     public static void main(String args[] ) throws Exception {
-    Scanner sc=new Scanner(System.in);
-    int t=sc.nextInt();
-    while(t-->0)
-    {
-        int n=sc.nextInt();
-        int k=sc.nextInt();
-
-      String array=sc.next();
-      int count=Integer.parseInt(array,2);
-      int cycle=0;
-      for(int i=0;i<n;i++)
-      {
-        String temp="";
-        for(int j=1;j<n;j++)
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
         {
-          temp+=array.charAt(j);
-          if(j==n-1)
+          long n=sc.nextLong();
+          long k=sc.nextLong();
+          sc.nextLine();
+          String arr=sc.nextLine();
+
+          String max=arr;
+          int cycle=0;
+          int p=-1;
+
+          for(int i=0;i<n;i++)
           {
-            temp+=array.charAt(0);
+              arr=arr.substring(1)+arr.charAt(0);
+            BigInteger maxx=new BigInteger(max,2);
+            BigInteger arrr=new BigInteger(arr,2);
+            if( maxx.compareTo(arrr)==-1){ max=arr; p=i;
+          //  System.out.printf("Case (%d) if -> cycle =%d p=%d max=%s arr=%s i= %d\n",t,cycle,p,max,arr,i);
           }
-        }
-        int max=Integer.parseInt(temp,2);
-        array=temp;
-        if(max>count)
-        {
-          count=max;
-          cycle=i+1;
-        }
-      }    
-      System.out.println(cycle*k+1);
-      
-    }
-    
-}
+            else if(maxx.compareTo(arrr)==0){ n=i-p; 
+            
+           // System.out.printf("Case (%d) else -> cycle =%d p=%d max=%s arr=%s i= %d\n",t,cycle,p,max,arr,i);
+            break;}
 
+          }
+
+            System.out.println(p+(k-1)*n+1);
+
+        }
+
+    }
 }
